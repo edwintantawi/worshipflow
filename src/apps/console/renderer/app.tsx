686d6d18bb031d40841a693b0e3401f2f79af981
edwintantawi <edwintantawi.dev@gmail.com>
2024-08-14 23:25:02 +0700

    feat: add ui library

--- src/apps/console/renderer/app.tsx
+++ src/apps/console/renderer/app.tsx
@@ -1,12 +1,23 @@
+import electronLogo from '~/assets/electron.svg';
+import { Button } from '~/components/ui/button';
 import { Versions } from '~/components/versions';
 
 export function App() {
   const versions = window.electron.process.versions;
+  const ipcHandle = () => window.electron.ipcRenderer.send('ping');
 
   return (
     <main className="h-screen grid place-items-center place-content-center gap-4 p-6">
+      <img alt="logo" className="w-20 h-20" src={electronLogo} />
       <h1 className="font-bold text-3xl">Worshipflow | Console App</h1>
-
+      <div className="space-x-2">
+        <Button asChild variant="outline">
+          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+            Documentation
+          </a>
+        </Button>
+        <Button onClick={ipcHandle}>Send IPC</Button>
+      </div>
       <Versions
         process={{
           chrome: versions.chrome,
